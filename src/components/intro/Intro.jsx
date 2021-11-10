a8a523db1a6f1b7ebcf9fb0253d4765b481cbfc3
Alex-parabol <68709781+Alex-parabol@users.noreply.github.com>
2021-11-10 22:01:55 +0100

    intro

--- src/components/intro/Intro.jsx
+++ src/components/intro/Intro.jsx
@@ -1,10 +1,36 @@
-import React from 'react'
+import React, {useEffect, useRef} from 'react'
+import { init } from 'ityped'
+
 import './intro.scss'
 
 export default function Intro() {
+  const textRef = useRef()
+  useEffect(()=> {
+    init(textRef.current, {
+      showCursor: true,
+      backDelay: 1000,
+      strings: ['Developer', 'Designer!','Playboy']
+    })
+  }, [])
+ 
+
     return (
         <div className='intro' id='intro'>
-          Intro  
+          <div className="left">
+            <div className="imgContainer">
+              <img src="assets/Alex-done-finally.png" alt="Foto de perfil" />
+            </div>
+          </div>
+          <div className="right">
+            <div className="wrapper">
+              <h2>Hi there, I'm</h2>
+              <h1>Alejandro Zamora</h1>
+              <h3>Web <span ref={textRef}></span></h3>
+            </div>
+            <a href="#portfolio">
+              <img src="assets/arrow.png" alt="" />
+            </a>
+          </div>
         </div>
     )
 }
