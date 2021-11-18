293804a6b53e38fe47a21ad53612ed2071acd4dd
Alex-parabol <68709781+Alex-parabol@users.noreply.github.com>
2021-11-18 23:01:20 +0100

    first media qry

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,10 +1,41 @@
-import React from 'react'
+import React,{useState} from 'react'
 import './contact.scss'
+import emailjs from 'emailjs-com'
 
 export default function Contact() {
+    
+    const [message, setMessage ] = useState(false)
+   
+
+    const sendEmail = (e) => {
+        e.preventDefault()
+        emailjs.sendForm('service_he7y9uu',
+        'template_6u2dpo7',
+        e.target,
+        'user_zICHsks0R6wDThW3oHeb3').then(res =>{
+            console.log(res);
+        }).catch(err=> console.log(err))
+        setMessage(true)
+    }
     return (
         <div className='contact' id='contact'>
-            Contact
+            <div className="left">
+                <img src="assets/handshake-medium.png" alt="Handshake logo" />
+            </div>
+            <div className="right">
+                <h2>Contact me!</h2>
+                <form onSubmit={sendEmail}>
+                    <input
+                    name='user_email'
+                    type="email"
+                    placeholder='Email'
+                    />
+                    <textarea name='message' placeholder='Message'></textarea>
+                    <button type='submit'>Send</button>
+                </form> 
+                {message ? <p>Thanks for your time. I will replay ASAP!</p> : null}
+            </div>
+           
         </div>
     )
 }
